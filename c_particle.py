b27230ae769d3d05352a897cf62220d9e06b8eb5
Kadykova Tatiana <tanya-kta@bk.ru>
2019-11-21 15:19:39 +0300

    transmission matrix added

--- c_particle.py
+++ c_particle.py
@@ -35,9 +35,17 @@ class Particle():
 		
 	def transmission_matrix(self, polyhedron):
 		C = np.zeros((3, 3))
+		verA = polyhedron.vertex[self.surface[0]]
+		verB = polyhedron.vertex[self.surface[1]]
+		verC = polyhedron.vertex[self.surface[2]]
+		C[:, 0] = verB - verA
+		C[:, 1] = verC - verA
+		C[0, 2] = C[1, 0]*C[2, 1] - C[2, 0]*C[1, 1]
+		C[1, 2] = C[2, 0]*C[0, 1] - C[0, 0]*C[2, 1]
+		C[2, 2] = C[0, 0]*C[1, 1] - C[1, 0]*C[0, 1]
+		return C
 		
-		
-		
+	
 		
 		
 	
