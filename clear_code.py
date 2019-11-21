428f7c2648fca84bc4c1295292b5d0dfa2db7650
pavTiger <pavTiger@gmail.com>
2019-11-21 12:32:54 +0300

    workin on plot

--- clear_code.py
+++ clear_code.py
@@ -1,6 +1,7 @@
 import numpy as np
 from vispy import app, scene
 from vispy import gloo
+from vispy import io, plot
 
 SIZE = (600, 600)
 TITLE = 'Physarum Polycephalum'
@@ -24,7 +25,10 @@ program['a_position'] = np.c_[
         np.random.uniform(-0.5, +0.5, 1000).astype(np.float32),
         np.random.uniform(-0.5, +0.5, 1000).astype(np.float32)]
 
-
+fname = 'teapot.obj' #io.load_data_file('teapot.obj')
+#fname = io.load_data_file('orig/triceratops.obj.gz')
+fig = plot.Fig()
+fig[1, 1].mesh(*io.read_mesh(fname)[:2])
 
 @c.connect
 def on_resize(event):
