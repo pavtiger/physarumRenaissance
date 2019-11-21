d88db6626c54b7b909fa52d1aa6fa90d6badf29a
Kadykova Tatiana <tanya-kta@bk.ru>
2019-11-21 19:11:05 +0300

    Space and face coordinates fixed

--- c_particle.py
+++ c_particle.py
@@ -1,4 +1,6 @@
 from additional import transmission_matrix, Point
+import random
+import numpy as np
     
 class Polyhedron():
     def __init__(self, verteces, faces):
@@ -30,30 +32,23 @@ class Particle():
         self.heading = heading
         self.surface = surface
         self.trail = np.zeros((5, 2))
-        self.trans_matrix = transmission_matrix(surface, polyhedron)
+        self.trans_matrix = np.array(transmission_matrix(surface, polyhedron))
     
     def count_step_size(self):
         self.SS = 1
         
     def space_to_face(self, p):
+        print(self.trans_matrix)
         new_p = Point(0, 0, 0)
-        new_p.x = p.x*self.trans_matrix[0, 0] + \
-                  p.y*self.trans_matrix[0, 1] + \
-                  p.z*self.trans_matrix[0, 2]
-        new_p.y = p.x*self.trans_matrix[1, 0] + \
-                  p.y*self.trans_matrix[1, 1] + \
-                  p.z*self.trans_matrix[1, 2]
-        new_p.z = p.x*self.trans_matrix[2, 0] + \
-                  p.y*self.trans_matrix[2, 1] + \
-                  p.z*self.trans_matrix[2, 2]
+        delta_p = np.array([p.x, p.y, p.z]) - np.array([self.coord.x, self.coord.y, self.coord.z])
+        new_p.x, new_p.y, new_p.z = self.trans_matrix @ delta_p
         return new_p
 
     def face_to_space(self, p):
         C = np.linalg.inv(self.trans_matrix)
         new_p = Point(0, 0, 0)
-        new_p.x = p.x*C[0, 0] + p.y*C[0, 1] + p.z*C[0, 2]
-        new_p.y = p.x*C[1, 0] + p.y*C[1, 1] + p.z*C[1, 2]
-        new_p.z = p.x*C[2, 0] + p.y*C[2, 1] + p.z*C[2, 2]
+        new_p.x, new_p.y, new_p.z = C@np.array([p.x, p.y, p.z]) + \
+                            np.array([self.coord.x, self.coord.y, self.coord.z])
         return new_p
 
     
@@ -62,27 +57,35 @@ class Particle():
         get polyhedron and TrailMap
         return Trail on the rigth and left sensors
         '''
-       # get coords of right sensor on (x,y,0) plain
-        rcoord = space_to_face(self.coord)# project point to (x,y,0) plain 
+        # get coords of right sensor on (x,y,0) plain
+        rcoord = space_to_face()   # project point to (x,y,0) plain 
         rcoord.x = rcoord.x + SO * np.sin(np.radians(heading + SA))
         rcoord.y = rcoord.y + SO * np.cos(np.radians(heading + SA))
         
-
         # get coords of left sensor on (x,y,0) plain
-        lcoord = space_to_face(self.coord)
+        сcoord = space_to_face()
+        сcoord.x = сcoord.x + SO * np.sin(np.radians(heading - SA))
+        сcoord.y = сcoord.y + SO * np.cos(np.radians(heading - SA))
+        
+        # get coords of left sensor on (x,y,0) plain
+        lcoord = space_to_face()
         lcoord.x = lcoord.x + SO * np.sin(np.radians(heading - SA))
         lcoord.y = lcoord.y + SO * np.cos(np.radians(heading - SA))
 
         # get 3D coord of sensors
         rsens = face_to_space(Points(rcoord.x, rcoord.y, 0))
+        сsens = face_to_space(Points(сcoord.x, сcoord.y, 0))
         lsens = face_to_space(Points(lcoord.x, lcoord.y, 0))
         
         return TrailMap[lsens.x, lsens.y, lsens.z], \
+               TrailMap[csens.x, csens.y, csens.z], \
                TrailMap[rsens.x, rsens.y, rsens.z]
-        pass
-
-        
         
     
     
     
+'''triangle = Polyhedron(verteces = [[0, 0, 1], [0, 1, 1], [1, 0, 2]], faces = [0, 1, 2])
+surface = [0, 1, 2]
+tanya = Point(1/3, 1/3, 4/3)
+part = Particle(0, 0, 1, surface, triangle)
+print(part.space_to_face(tanya).x, part.space_to_face(tanya).y, part.space_to_face(tanya).z)'''
