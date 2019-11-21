ba5487839753f4a3b78fac824712667d255c95d4
OlgaStarunova <bogadelenka@mail.ru>
2019-11-21 15:44:52 +0300

    Merge conflict fixed, sense_trail added

--- c_particle.py
+++ c_particle.py
@@ -1,8 +1,20 @@
 class Point():
-	def __init__(self, x, y, z):
-		self.x = x
-		self.y = y
-		self.z = z
+    def __init__(self, x, y, z):
+        self.x = x
+	self.y = y
+	self.z = z
+
+def transmission_matrix(surface, polyhedron):
+    C = np.zeros((3, 3))
+    verA = polyhedron.vertex[surface[0]]
+    verB = polyhedron.vertex[surface[1]]
+    verC = polyhedron.vertex[surface[2]]
+    C[:, 0] = verB - verA
+    C[:, 1] = verC - verA
+    C[0, 2] = C[1, 0]*C[2, 1] - C[2, 0]*C[1, 1]
+    C[1, 2] = C[2, 0]*C[0, 1] - C[0, 0]*C[2, 1]
+    C[2, 2] = C[0, 0]*C[1, 1] - C[1, 0]*C[0, 1]
+    return C
 
 class Particle():
     def __init__(self, x, y, z, heading = random.randint(0, 360), surface):
@@ -29,23 +41,38 @@ class Particle():
         self.heading = heading
         self.surface = surface
         self.trail = np.zeros((5, 2))
+        self.trans_matrix = transmission_matrix(surface, polyhedron)
     
     def count_step_size(self):
         self.SS = 1
 		
-	def transmission_matrix(self, polyhedron):
-		C = np.zeros((3, 3))
-		verA = polyhedron.vertex[self.surface[0]]
-		verB = polyhedron.vertex[self.surface[1]]
-		verC = polyhedron.vertex[self.surface[2]]
-		C[:, 0] = verB - verA
-		C[:, 1] = verC - verA
-		C[0, 2] = C[1, 0]*C[2, 1] - C[2, 0]*C[1, 1]
-		C[1, 2] = C[2, 0]*C[0, 1] - C[0, 0]*C[2, 1]
-		C[2, 2] = C[0, 0]*C[1, 1] - C[1, 0]*C[0, 1]
-		return C
-		
-	
+    def space_to_face(self)
+    
+
+    
+    def sense_trail(self, polyhedron, TrailMap):
+        '''
+        get polyhedron and TrailMap
+        return Trail on the rigth and left sensors
+        '''
+        # get coords of right sensor on (x,y,0) plain
+        rcoord = space_to_face(self.coord)# project point to (x,y,0) plain 
+        rcoord.x = fcoord.x + SO * np.sin(np.radians(RA + SA))
+        rcoord.y = fcoord.y + SO * np.cos(np.radians(RA + SA))
+        
+
+        # get coords of left sensor on (x,y,0) plain
+        lcoord = space_to_face(self.coord)
+        lcoord.x = lcoord.x + SO * np.sin(np.radians(RA - SA))
+        lcoord.y = lcoord.y + SO * np.cos(np.radians(RA - SA))
+
+        # get 3D coord of sensors
+        rsens = face_to_space(Points(rcoord.x, rcoord.y, 0)
+	lsens = face_to_space(Points(lcoord.x, lcoord.y, 0)
+
+	return TrailMap[lsens.x, lsens.y, lsens.z], \
+               TrailMap[rsens.x, rsens.y, rsens.z]
+
 		
 		
 	
