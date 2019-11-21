1651e18061e6490f9f8d9c6304bdc7f4e64669a5
pavTiger <pavTiger@gmail.com>
2019-11-21 17:31:26 +0300

    2x renassense

--- main.py
+++ main.py
@@ -7,19 +7,24 @@ SIZE = (600, 600)
 TITLE = 'Physarum Polycephalum'
 SIZE = 0.1
 
-fname1 = 'teapot.obj'
-fname2 = 'sphere.obj'
-fig = plot.Fig(title=TITLE)
-fig[0, 0].mesh(*io.read_mesh(fname1)[:2])
-    
-arr = io.read_mesh(fname2)[:2]
-for elem in arr[0]:
-    elem[0] = elem[0] * SIZE
-    elem[1] = elem[1] * SIZE
-    elem[2] = elem[2] * SIZE
-    
-fig[0, 0].mesh(*arr, color=(1, 1, 0.1))
-app.run()
+def drawOutput(organism):
+    fname1 = 'teapot.obj'
+    fname2 = 'sphere.obj'
+    fig = plot.Fig(title=TITLE)
+    fig[0, 0].mesh(*io.read_mesh(fname1)[:2])
+        
+    arr = io.read_mesh(fname2)[:2]
+    for dot in organism:
+        a = arr
+        for elem in a[0]:
+            elem[0] = elem[0] * 1 + dot[0]
+            elem[1] = elem[1] * 1 + dot[1]
+            elem[2] = elem[2] * 1 + dot[2]
+        #print('workin')
+    fig[0, 0].mesh(*arr, color=(1, 1, 0.1))
+            
+    app.run()
 
 if __name__ == "__main__":
-    drawOutput([[0, 0, 0], [0.5, 0, 0.5], [0.5, 0, 0], [0, 0, 0.5], [1, 0, 0]])
+    print('hey')
+    drawOutput([[0, 0, 0.5], [0, 0, 0.5], [0, 0, 0], [0.5, 0, 0], [0, 0, 0]])
