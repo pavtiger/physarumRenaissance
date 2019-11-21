0fe7b704a32f6b03d014c7a91cd2a12d34150186
Kadykova Tatiana <tanya-kta@bk.ru>
2019-11-21 13:59:59 +0300

    Class Point added

--- c_particle.py
+++ c_particle.py
@@ -1,3 +1,9 @@
+class Point():
+	def __init__(self, x, y, z):
+		self.x = x
+		self.y = y
+		self.z = z
+
 class Particle():
     def __init__(self, x, y, z, heading = random.randint(0, 360), surface):
         """
@@ -9,7 +15,7 @@ class Particle():
             param depT: trail distance
             param x, y, z: agent's coordinates
             param heading: agent's angle relative to its surface
-            param surface: agent surface coefficients
+            param surface: 3 vertexes of polyhedron
             param trails: array with trail's coordinates
         """
         self.SA = 45
@@ -19,12 +25,23 @@ class Particle():
         self.depT = 5
         self.food = 255
         self.foodTrH = 20
-        self.x = x
-        self.y = y
-        self.z = z
+        self.coord = Point(x, y, z)
         self.heading = heading
         self.surface = surface
         self.trail = np.zeros((5, 2))
     
-    def count_step_size()
+    def count_step_size(self):
         self.SS = 1
+		
+	def transmission_matrix(self, polyhedron):
+		C = np.zeros((3, 3))
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
